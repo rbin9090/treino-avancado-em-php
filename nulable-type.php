b484d2bd5c81a7ef336a0c6a235038b48d953af9
Robson Barros <robsonfullstackweb@gmail.com>
2023-03-06 10:16:27 -0300

    nulable-type metodo de declarar o que quero que me  retorne

---
+++ nulable-type.php
@@ -0,0 +1,18 @@
+<?php 
+
+function teste() :?string{
+   return 9;
+}
+
+$numero = teste();
+
+var_dump($numero);
+
+/*
+metododo php7 fectures
+metodos pra odeclar o que quero que me retorne */
+
+
+
+
+?>
